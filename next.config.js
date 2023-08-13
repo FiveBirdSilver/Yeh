37214d3054de1147e8dadea0a0e1ad74fdb6230d
fivebirdsilver <tpdms0401@gmail.com>
2023-08-13 20:31:46 +0900

    도커 파일 및 CI / CD 제거

--- next.config.js
+++ next.config.js
@@ -1,27 +1,27 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require("path");
 const nextConfig = {
   reactStrictMode: false,
-  output: 'export',
+  output: "export",
 
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
 
-  images: {
-    domain : ['yeh-bucket.s3.ap-northeast-2.amazonaws.com'],
-    unoptimized : true
-  },
+  // images: {
+  //   domain : ['yeh-bucket.s3.ap-northeast-2.amazonaws.com'],
+  //   unoptimized : true
+  // },
 
   async redirects() {
     return [
       {
         source: "/",
         destination: "/main",
-        permanent: false
-      }
-    ]
-  }
-}
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
